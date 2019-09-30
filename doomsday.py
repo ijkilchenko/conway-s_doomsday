d4055af47142853fea5b9b6c1abf027942700930
Alex Ilchenko <ijkilchenko@gmail.com>
2019-09-30 16:20:41 -0700

    Typo

--- doomsday.py
+++ doomsday.py
@@ -8,7 +8,7 @@ def get_random_date():
     end = datetime.datetime.today()
     return start + timedelta(days=randint(0, int((end - start).days)))
 
-num_to_month = {1: 'Januray', 2: 'February', 3: 'March',
+num_to_month = {1: 'January', 2: 'February', 3: 'March',
                 4: 'April', 5: 'May', 6: 'June',
                 7: 'July', 8: 'August', 9: 'September',
                 10: 'October', 11: 'November', 12: 'December'}
